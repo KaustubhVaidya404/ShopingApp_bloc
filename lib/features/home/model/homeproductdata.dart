a8cdd08d35b156424019da0db1f1cff0b7694e9b
Kaustubh <pro.vaidya4413@gmail.com>
2023-10-14 15:44:46 +0530

    home page completed

--- lib/features/home/model/homeproductdata.dart
+++ lib/features/home/model/homeproductdata.dart
@@ -1,11 +1,13 @@
-class ProductData {
+
+
+class ProductDataModel {
   final String id;
   final String name;
   final String description;
   final double price;
   final String imageurl;
 
-  ProductData({
+  ProductDataModel({
     required this.id,
     required this.name,
     required this.description,
